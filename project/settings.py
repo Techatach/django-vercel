ba77cf8942d338a25b36d2092362adabbb943b63
Techatach <techatach@gmail.com>
2025-06-29 11:55:01 +0700

    second commit

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # pip install crispy-bootstrap5
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
